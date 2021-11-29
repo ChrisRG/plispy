f0f37c250b79a05f7ec9b6e0469f1caf4fd1b151
Chris Geekie <chris.geekie@gmail.com>
2021-11-29 16:10:27 +0100

    Basic parsing

--- src/plispy/plispy.py
+++ src/plispy/plispy.py
@@ -1,8 +1,37 @@
 ### Types
 Symbol = str
-List = list
 Number = (int, float)
+Atom = (Symbol, Number)
+List = list
+Exp = (Atom, List)
+Env = dict
 
-### Parser
+### Tokenizer
 def tokenize(chars: str) -> list:
     return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
+### Parser
+def parse(program: str):
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens:list):
+    if len(tokens) == 0:
+        raise SyntaxError('Unexpected EOF')
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)
+        return L
+    elif token == ')':
+        raise SyntaxError('Unexpected )')
+    else:
+        return atom(token)
+
+def atom(token: str):
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError:
+            return Symbol(token)
